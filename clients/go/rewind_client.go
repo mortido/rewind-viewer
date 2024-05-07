a1fce226eb9b98d3cdf19eebf497ca099b798b39
mortido <open.for.good@gmail.com>
2024-05-08 00:32:52 +0200

    Clients templates generated with chat gpt.

--- clients/go/rewind_client.go
+++ clients/go/rewind_client.go
@@ -69,9 +69,3 @@ func (c *RewindClient) send(buf []byte) {
 	c.conn.Write(header)
 	c.conn.Write(buf)
 }
-
-func reverse(bytes []byte) {
-	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
-		bytes[i], bytes[j] = bytes[j], bytes[i]
-	}
-}
